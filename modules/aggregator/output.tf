a228f1649ad44bffd711410e6cd2ed2a1ddc6f11
Vishal Ahir <vishalmakahir112233@gmail.com>
2022-02-28 12:12:21 +0530

    Added new line at last of the file

--- modules/aggregator/output.tf
+++ modules/aggregator/output.tf
@@ -4,6 +4,6 @@ output "config_aggregator_arn" {
 }
 
 output "account_ids" {
-  value = data.aws_organizations_organization.example.accounts[*].id
+  value       = data.aws_organizations_organization.example.accounts[*].id
   description = "Account ids of all accounts under the main organization account"
 }
